e947f4c7965c5a4b31c130c85c93ce4b02d5bf61
JDM Ubuntu VB <JKDMarks@gmail.com>
2019-04-09 15:24:56 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+
+  attr_accessor :string
+
+  def initialize(string)
+    @string = string
+  end
+
+  def match(array)
+    array.select do |word|
+      self.string.split("").sort == word.split("").sort
+    end
+  end
+end
